2a115f07dd066db6e87729d8a1421c6a2ed361aa
Robin Hood <rhcrco@gmail.com>
2026-05-02 15:25:04 -0400

    change script to use pyspark and create a file.

--- process_people.py
+++ process_people.py
@@ -1,12 +1,35 @@
-import csv
+import argparse
 
-with open("people.csv", mode="r", newline="") as file:
-    reader = csv.DictReader(file)
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, upper
 
 
-    for row in reader:
-        name = row["name"]
-        role = row["role"]
+parser = argparse.ArgumentParser()
 
-        sentence = f"{name} works as a {role.upper()}."
-        print(sentence)
+parser.add_argument(
+    "--input-path",
+    default="dbfs:/tmp/people_from_notebook_csv"
+)
+
+args = parser.parse_args()
+
+spark = SparkSession.builder.getOrCreate()
+
+df = (
+    spark.read
+    .option("header", "true")
+    .option("inferSchema", "true")
+    .csv(args.input_path)
+)
+
+processed_df = (
+    df
+    .withColumn("name_uppercase", upper(col("name")))
+    .withColumn("age_next_year", col("age") + 1)
+)
+
+print("Original DataFrame from notebook:")
+df.show()
+
+print("Processed DataFrame from Python script:")
+processed_df.show()
